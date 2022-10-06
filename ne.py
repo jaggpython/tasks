0ff38e1a10672603928aed2a6bffed0f4d1723ed
Python Developer-0.1 <79072285+jaggpython@users.noreply.github.com>
2022-10-06 18:55:53 +0530

    initial commit
    commit change

--- ne.py
+++ ne.py
@@ -8,12 +8,15 @@ def freq(str):
             fre[i]=1
     new=sorted(fre.items(),key=lambda x:x[1],reverse=True)
     new1=sorted(fre.items(),key=lambda x:x[1])
-    print('\n Descending order:',end="")
-    print(dict(new))
+
+    print('\n Descending order:',dict(new))
     print('\n Ascending Order:',dict(new1))
     print('\n')
 
 str="new world of india india new new "
 freq(str)
+
+
+
 
 
