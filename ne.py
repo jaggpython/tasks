fc3aed37c008db9bc17f4756c6542507aa776fb4
Python Developer-0.1 <79072285+jaggpython@users.noreply.github.com>
2022-10-06 18:46:08 +0530

    initial commit
    commit change

--- ne.py
+++ ne.py
@@ -1,22 +1,20 @@
+def freq(str):
+    str=str.split()
+    fre={}
+    for i in str:
+        if i in fre:
+            fre[i]+=1
+        else:
+            fre[i]=1
+    new=sorted(fre.items(),key=lambda x:x[1],reverse=True)
+    new1=sorted(fre.items(),key=lambda x:x[1])
+
+    print('\n Descending order:',end="")
+    print(dict(new))
+    print('\n Ascending Order:',dict(new1))
+    print('\n')
+
 str="new world of india india new new "
-
-str=str.split()
-
-fre={}
-
-for i in str:
-    if i in fre:
-        fre[i]+=1
-    else:
-        fre[i]=1
-
-new=sorted(fre.items(),key=lambda x:x[1],reverse=True)
-new1=sorted(fre.items(),key=lambda x:x[1])
-
-print('\n Descending order:',end="")
-print(dict(new))
-print('\n')
-print('\n Ascending Order:',dict(new1))
-print('\n')
+freq(str)
 
 
